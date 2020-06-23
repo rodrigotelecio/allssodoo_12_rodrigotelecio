c4d8d0b917198d22990c08c5b8ab35bfe7fde654
Rodrigo de Oliveira Telecio da Silva <66520213+rodrigotelecio@users.noreply.github.com>
2020-06-23 10:27:21 -0300

    Add files via upload

---
+++ br_nfse_susesu/models/res_company.py
@@ -0,0 +1,12 @@
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    # Nota Susesu
+    senha_ambiente_nfse = fields.Char(
+        string=u'Senha NFSe', size=30, help=u'Senha Nota Fiscal de Serviço')
