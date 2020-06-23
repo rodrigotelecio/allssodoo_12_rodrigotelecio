edf725e2e26a1823570c2d98bd5ef363d5cfd18f
Rodrigo de Oliveira Telecio da Silva <66520213+rodrigotelecio@users.noreply.github.com>
2020-06-23 10:12:14 -0300

    Add files via upload

---
+++ br_account_tax_python/models/account_invoice.py
@@ -0,0 +1,19 @@
+# © 2019 Mackilem Van der Laan, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    def _prepare_tax_context(self):
+        res = super(AccountInvoiceLine, self)._prepare_tax_context()
+        lines = self.invoice_id.invoice_line_ids
+        amount_total = sum((l.quantity * l.price_unit) +
+                           l.price_tax - l.valor_desconto
+                           for l in lines)
+        amount = (amount_total
+                  + self.quantity * self.price_unit)
+        res['invoice_amount'] = amount
+        return res
